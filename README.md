dcd21cef64e3e2f7aa675c0952e546c1650092b2
TDK <name20411@gmail.com>
2025-03-07 06:58:45 -0500

    added fps counter

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Intro
 
-    This is a Basic Demo of the Ebitengine for the *Go* language.
+This is a Basic Demo of the Ebitengine for the *Go* language.
 
-    I intend this demo to be brief and simply demonstrate to myself that this engine runs on my machine; though I might want to take things further with future branching out.
-    
+I intend this demo to be brief and simply demonstrate to myself that this engine runs on my machine; 
+though I might want to take things further with future branching out.
+
+the end goal is for a solid set of experimentation to have taken place;
--- main.go
+++ main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
-
-func (g *Game) Update() error {
-	return nil
+type FrameRateTracking struct {
+	fRate  float64
+	lTime  time.Time
+	frames int
 }
 
+type Game struct {
+	//game variables and other such things go here;
+	fRate  float64
+	lTime  time.Time
+	frames int
+}
+
+func (g *Game) Update() error {
+	//game logic goes here;
+	//this might be the basic CPU-type logic only though... not sure;
+	return nil
+}
+func (g *Game) FPSChanger() {
+	tempTime := time.Since(g.lTime)
+	if tempTime.Milliseconds() > 250 {
+		g.fRate = (float64(g.frames) / tempTime.Seconds())
+		g.frames = 0
+		g.lTime = time.Now()
+	} else {
+
+		g.frames += 1
+	}
+}
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	//this might be graphic layout?? a means perhaps to control layers?
+	//ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS:%3.1f\nA:%3.1f\nFRAMES:%d", g.fRate, g.fRateAvg, g.frames))
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS:%3.1f", g.fRate))
+	g.FPSChanger()
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -22,9 +50,17 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
+	//initializing
+	var g = Game{
+		fRate:  0.0,
+		lTime:  time.Now(),
+		frames: 0,
+	}
 	ebiten.SetWindowSize(640, 480)
+	// presentTime := time.Now()
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	//this is where the game logic runs
+	if err := ebiten.RunGame(&g); err != nil {
 		log.Fatal(err)
 	}
 }
